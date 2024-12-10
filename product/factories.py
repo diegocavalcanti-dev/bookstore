b8145704384ed11106d97808a3667b53f9b467c8
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-12-09 22:29:50 -0300

    config e clean up

--- product/factories.py
+++ product/factories.py
@@ -2,7 +2,6 @@ import factory
 
 from product.models import Category, Product
 
-
 class CategoryFactory(factory.django.DjangoModelFactory):
     title = factory.Faker("pystr")
     slug = factory.Faker("pystr")
